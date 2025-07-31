df8164e3ed764c2fb93678466d8eddafca59d806
Geral <geralramirezc2004@gmail.com>
2025-07-31 12:35:07 -0500

    repositorio js version 1

---
+++ Logica-javascript-3168940/1_sintaxis_variables/app.js
@@ -0,0 +1,24 @@
+//El alert muestra una ventana emergente en el navegador
+//alert("hola mundo")
+//prompt muetra una caja para ingresar informacion ddesde el navegador 
+//prompt("dime tu nombre")
+//variable declarada con let
+let personaje="perrito"
+//crear variable puntaje y asingnar valor de 0
+let puntaje= 0
+//crear variable shots y asingnr valor de tres
+let shots= 3
+   recarga= 2
+console.log("total balas:",shots + recarga)
+
+//El usuario capturo cuatro patos
+puntaje +=4
+//El ususario gastó 
+shots-= 2
+//El usuario recargo su arma
+recarga+= 1
+recarga++//++permite sumar de a uno
+
+console.log("puntaje", puntaje) //4
+console.log("shots", shots)//1
+console.log("recarga", recarga)//3
---
+++ Logica-javascript-3168940/2_operadores_comparacion _logicos/app.js
@@ -0,0 +1,21 @@
+//boolean(...)
+
+console.log(' "5" == "5" ', 5 == 5) // true
+console.log(' "5" === "5" ', "5" === "5") //true
+console.log(' 5 === "5" ', 5 === "5") //false
+console.log(' 5 == "5" ', 5 == "5") // true
+console.log(' 5 === (3+2) ', "5" === (3+2) ) //false
+console.log(' 5 === 5 ', 5 === 5) // true
+
+//concatenar (+)
+console.log(5 + "3")// 53
+console.log(5 + Number ("3") )// 8
+console.log(5 * "3")// 15
+console.log(5 + 3)// 8
+
+//pida dos numeros al usuario y sumelos
+let num1= prompt('Dame num1') //"5"
+let num2= prompt('Dame num2')//"3"
+let r= num1 + num2
+alert("El resultado de tu suma es" + resultado)
+
