b027b873e92711a7a7401ebc1765d8d295ca4cab
Geral <geralramirezc2004@gmail.com>
2025-07-31 13:28:28 -0500

    ejercicio terminado

--- Logica-javascript-3168940/2_operadores_comparacion _logicos/app.js
+++ Logica-javascript-3168940/2_operadores_comparacion _logicos/app.js
@@ -14,8 +14,22 @@ console.log(5 * "3")// 15
 console.log(5 + 3)// 8
 
 //pida dos numeros al usuario y sumelos
-let num1= prompt('Dame num1') //"5"
-let num2= prompt('Dame num2')//"3"
-let r= num1 + num2
-alert("El resultado de tu suma es" + resultado)
+//let num1= prompt('Dame num1') //"5"
+//let num2= prompt('Dame num2')//"3"
+//let resultado= num1 + num2
+//alert("El resultado de tu suma es" + resultado)
 
+//pedir al usuario valor 1 y 2
+let valo1= parseFloat(prompt("dame valor de 1"))
+let valo2= parseFloat(prompt("dame valor de 2"))
+
+//imprimir valores ingresados
+console.log('el valor de 1 es = ${valor1} y el valor de 2 es = ${valor2}');
+
+//operaciones
+console.log('{la suma de "{$valor1}" + "${valor2}" ="${valor1+valor2}');
+console.log('{la resta de "{$valor1}" - "${valor2}" ="${valor1-valor2}');
+console.log('{la multiplicacion de "{$valor1}" * "${valor2}" = "${valor1*valor2}');
+console.log('{la suma de "{$valor1}" / "${valor2}" ="${valor1/valor2}');
+
+console.log(`tu número es $valor1{num1}`)
