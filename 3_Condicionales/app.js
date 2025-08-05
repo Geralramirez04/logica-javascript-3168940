f7d3fd914e037f8bb872408a9f3a11bad345b121
Geral <geralramirezc2004@gmail.com>
2025-08-05 13:13:35 -0500

    Ejercicio 1 y 2 condicionales

---
+++ 3_Condicionales/app.js
@@ -0,0 +1,29 @@
+//ejercicio javascript 1//
+
+let nombre=propmt("Ingresa tu nombre");
+let edad=propmt("Ingresa tu edad");
+
+if(nombre === "Carlos" || nombre === "Mario");
+    alert("Puedes ingresar al VIP")
+
+    alert("Oh, no puedes pasar, lo siento");     
+    
+ 
+if(edad >=18) {
+    alert("Eres mayor de edad puedes pasar");
+}
+else alert("Eres menor de edad, no puedes pasar");
+
+//ejercicio javascript 2//
+let estatura = parseInt(propmt("¿Cuál es tu estatura?"));
+let velocidad = parseInt(propmt("¿Cuál es tu velocidad en km/h?"));
+let Edad = parseInt(propmt("¿Cuál es tu edad?"));
+
+if(estatura >=170 && velocidad >= 27 ) {
+    alert("Felicidades, puedes ingresar al equipo");
+}
+if(edad < 18){
+    alert("Eres menor de edad, puedes entrar en la division juvenil");
+} else
+    alert("Eres mayor de edad, puedes ingresar a la división adulta");
+
