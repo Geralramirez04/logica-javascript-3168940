1a4861cb4211bd288fcbd196f8d2fa86ef5e449e
Geral <geralramirezc2004@gmail.com>
2025-08-12 13:13:10 -0500

    Add files via upload

---
+++ 4_Ciclos/app.js
@@ -0,0 +1,35 @@
+
+director: "robert zemeckis"
+console.log
+
+/let num= Number(prompt("Inroduce un Número"))
+/let counter
+
+while(counter<= nun){
+ if(counter %5===0){
+      console.log(counter)
+    }
+   counter++
+   }
+    
+    //////////
+    for(let i = 1; i<=50; i++){
+    if(i % 2 != 0){
+        console.log(i)
+     }
+    }
+
+
+    //cre un ciclo for que imprima la tabla del 7
+  ///console.log(7 * i)
+
+  for(let i = 7; i<= 50; i++){
+   console.log("7 x ${i} = ${7*1}")
+    
+  }
+  for(let i= 1; i<= 100; i++){
+    if(i >= 20 && i >=30){
+        continue
+    }
+    console.log(i)
+  }
